e89ba665ba0ed07f542273692639b6a97734b987
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-29 07:54:56 +0200

    add scale factor in vertex shader to scale image with plane

--- Image Processing Methods/Image Transformation/assets/js/shaders.js
+++ Image Processing Methods/Image Transformation/assets/js/shaders.js
@@ -1,10 +1,12 @@
 const vertexShader = 
     `
     varying vec2 vUv;
+    uniform float scaleFactor;
+
     void main() {
       vUv = vec2( uv.x, 1.0-uv.y );
       gl_Position = projectionMatrix *
-        modelViewMatrix * vec4(position, 1.0 );
+        modelViewMatrix * vec4(scaleFactor * position, 1.0 );
     }
 `
 
