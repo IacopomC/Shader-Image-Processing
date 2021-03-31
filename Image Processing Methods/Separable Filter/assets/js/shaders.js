8907a59afd04399c06afe4bcf21e83d803dc1f68
iacopo.catalano <iacopomcatalano@gmail.com>
2021-03-31 18:37:13 +0200

    change gaussian filter to separable

--- Image Processing Methods/Separable Filter/assets/js/shaders.js
+++ Image Processing Methods/Separable Filter/assets/js/shaders.js
@@ -33,12 +33,14 @@ const fragmentShader =
       
       // rows
       for (float i = -k; i <= k; i++) {
-        for (float j = -k; j <= k; j++) {
-          float y = uv.y + float(j) * cellSize.y;
-					textureValue +=
-                texture2D( image, uv + vec2( float(i)*cellSize.x, float(j)*cellSize.y ) ) *
-                exp(-(i*i + j*j)/(2.0*sigma*sigma));
-        }
+        float x = uv.x + float(i) * cellSize.x;
+        textureValue += texture2D(image, vec2(x, uv.y)) * exp(-(i*i)/(2.0*sigma*sigma));
+      }
+
+      // columns
+      for (float j = -k; j <= k; j++) {
+        float y = uv.y + float(j) * cellSize.y;
+        textureValue += texture2D(image, vec2(uv.x, y)) * exp(-(j*j)/(2.0*sigma*sigma));
       }
       
       textureValue /= (sqrt(2.0*Pi)*sigma*sigma);
