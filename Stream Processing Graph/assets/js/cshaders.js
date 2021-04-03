edafe85f212f33df7527c082d7aa2845dde856e4
iacopo.catalano <iacopomcatalano@gmail.com>
2021-04-03 18:23:48 +0200

    add combination and scaling shaders files

---
+++ Stream Processing Graph/assets/js/cshaders.js
@@ -0,0 +1,54 @@
+const cvertexShader = 
+    `
+    varying vec2 vUv;
+
+    void main() {
+      vUv = vec2( uv.x, 1.0-uv.y );
+      gl_Position = projectionMatrix *
+        modelViewMatrix * vec4(position, 1.0 );
+    }
+`
+
+const cfragmentShader = 
+    `
+    precision highp float;
+
+    const int kernelSizeDiv2 = 2;
+    
+    uniform sampler2D image;
+    uniform sampler2D image2;
+    uniform vec2 resolution;
+    uniform int operator;
+    uniform float scaleFactor;
+    uniform float offset;
+    
+    varying vec2 vUv;
+    
+    void main(void) {
+      vec2 cellSize = 1.0 / resolution.xy;
+      vec2 uv = vUv.xy;
+    
+      vec3 textureValue = vec3(0, 0, 0);
+      vec3 textureValue2 = vec3(0, 0, 0);
+      for (int i = -kernelSizeDiv2; i <= kernelSizeDiv2; i++){
+        for (int j = -kernelSizeDiv2; j <= kernelSizeDiv2; j++) {
+          textureValue += texture2D(image, uv + vec2(float(i) * cellSize.x, float(j) * cellSize.y)).rgb;
+          textureValue2 += texture2D(image2, uv + vec2(float(i) * cellSize.x, float(j) * cellSize.y)).rgb;
+        }
+      }
+      if(operator == 0) {
+        textureValue += textureValue2;
+      } else if (operator == 1) {
+        textureValue -= textureValue2;
+      } else if (operator == 2) {
+        textureValue *= textureValue2;
+      } else if (operator == 3) {
+        textureValue /= textureValue2;
+      }
+      textureValue = (textureValue * scaleFactor) + offset;
+      textureValue /= float((kernelSizeDiv2 * 2 + 1) * (kernelSizeDiv2 * 2 + 1));
+      gl_FragColor = vec4 (textureValue, 1.0);
+    }
+    `
+
+export {cvertexShader, cfragmentShader}
---
+++ Stream Processing Graph/assets/js/scshaders.js
@@ -0,0 +1,42 @@
+const scvertexShader = 
+    `
+    varying vec2 vUv;
+
+    void main() {
+      vUv = vec2( uv.x, 1.0-uv.y );
+      gl_Position = projectionMatrix *
+        modelViewMatrix * vec4(position, 1.0 );
+    }
+`
+
+const scfragmentShader = 
+    `
+    precision highp float;
+
+    const int kernelSizeDiv2 = 2;
+    
+    uniform sampler2D image;
+    uniform vec2 resolution;
+    
+    varying vec2 vUv;
+    
+    void main(void) {
+      vec2 cellSize = 1.0 / resolution.xy;
+      vec2 uv = vUv.xy;
+        
+      vec4 color00 = texture2D ( image, uv - vec2( cellSize.x, cellSize.y ) );
+      vec4 color01 = texture2D ( image, uv + vec2( - cellSize.x, cellSize.y ) );
+      vec4 color10 = texture2D ( image, uv + vec2( cellSize.x, - cellSize.y ) );
+      vec4 color11 = texture2D ( image, uv + vec2( cellSize.x, cellSize.y ) );
+
+      float x_diff = cellSize.x;
+      float y_diff = cellSize.y;
+
+      vec4 texColor = color01*(1.0-x_diff)*(1.0-y_diff) +  color11*(x_diff)*(1.0-y_diff)
+                        + color00*(y_diff)*(1.0-x_diff) +  color10*(x_diff*y_diff);
+
+      gl_FragColor = texColor;
+    }
+    `
+
+export {scvertexShader, scfragmentShader}
