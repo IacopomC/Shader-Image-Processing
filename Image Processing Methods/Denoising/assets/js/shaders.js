a1037dcc2a10deb5f127051cfd8d3492cbc263d9
iacopo.catalano <iacopomcatalano@gmail.com>
2021-04-03 19:54:45 +0200

    implement sort in a function

--- Image Processing Methods/Denoising/assets/js/shaders.js
+++ Image Processing Methods/Denoising/assets/js/shaders.js
@@ -23,6 +23,26 @@ const fragmentShader =
     float rNeighbors[MAX_SIZE];
     float gNeighbors[MAX_SIZE];
     float bNeighbors[MAX_SIZE];
+
+    float[MAX_SIZE] sortArray(float array[MAX_SIZE], int counter){
+      for(int i = 0; i < counter; i++){
+     
+        // Last i elements are already in place  
+        for(int j = 0; j < ( counter - i -1 ); j++){
+            
+          // Checking if the item at present iteration 
+          // is greater than the next iteration
+          if(array[j] > array[j+1]){
+              
+            // If the condition is true then swap them
+            float temp = array[j];
+            array[j] = array[j + 1];
+            array[j+1] = temp;
+          }
+        }
+      }
+      return array;
+    }
     
     void main(void) {
 
@@ -48,39 +68,12 @@ const fragmentShader =
         }
       }
 
-      for(int i = 0; i < counter; i++){
-     
-        // Last i elements are already in place  
-        for(int j = 0; j < ( counter - i -1 ); j++){
-            
-          // Checking if the item at present iteration 
-          // is greater than the next iteration
-          if(rNeighbors[j] > rNeighbors[j+1]){
-              
-            // If the condition is true then swap them
-            float temp = rNeighbors[j];
-            rNeighbors[j] = rNeighbors[j + 1];
-            rNeighbors[j+1] = temp;
-          }
-
-          if(gNeighbors[j] > gNeighbors[j+1]){
-              
-            // If the condition is true then swap them
-            float temp = gNeighbors[j];
-            gNeighbors[j] = gNeighbors[j + 1];
-            gNeighbors[j+1] = temp;
-          }
-
-          if(bNeighbors[j] > bNeighbors[j+1]){
-              
-            // If the condition is true then swap them
-            float temp = bNeighbors[j];
-            bNeighbors[j] = bNeighbors[j + 1];
-            bNeighbors[j+1] = temp;
-          }
-        }
-      }
-
+      rNeighbors = sortArray(rNeighbors, counter);
+      
+      gNeighbors = sortArray(gNeighbors, counter);
+      
+      bNeighbors = sortArray(bNeighbors, counter);
+      
       int medianIndx = 0;
 
       if (counter % 2 == 0){
